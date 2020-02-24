28cbe940e986fbbef4882df996cf4e5d70b5435d
Justin Mai <justintmai@gmail.com>
2020-02-24 03:27:20 -0800

    (WIP) Open Solidworks

--- main.js
+++ main.js
@@ -8,25 +8,64 @@ var helloWorld = edge.func(function () {/*
   #r "C:\node-solidworks\SwCommands.dll"
   #r "C:\node-solidworks\SwConst.dll"
 
+  using System.Threading.Tasks;  // for `async Task`
+  using System.IO;  // for `FileInfo(filepath)`
+  using System;  // for `Console.WriteLine()` to write to console
+
   using SwConst;
   using SwCommands;
   using SldWorks;
-  using System.Threading.Tasks;
 
     public class Startup
     {
+      string filepath = "C:\\node-solidworks-master\\SimpleRectangle.SLDPRT";
+      private static SldWorks.SldWorks swApp;
+
       public async Task<object> Invoke(object input)
       {
-          SldWorks.SldWorks swApp;
-          GetSolidworksModelFromFile('')
-          return null;
+
+        swApp = new SldWorks.SldWorks();
+        OpenSolidworks();
+        GetSolidworksModelFromFile(filepath);
+        return null;
       }
 
 
-      // Opens and returns a model, given a file path. Returns currently-active model if already open
+      // Opens and returns a model, given a file path.
+      //    Returns currently-active model if already open.
       public ModelDoc2 GetSolidworksModelFromFile(string filePath)
       {
-          return null;
+        // Declare variables
+        ModelDoc2 currentModel = default(ModelDoc2);
+        string fileName = new FileInfo(filePath).Name;
+        int i_errors = 0;
+        int i_warnings = 0;
+
+        // Check if Solidworks.exe is open
+            if (swApp == null)
+            {
+              OpenSolidworks();
+            }
+
+        return null;
+      }
+
+
+      // Open Solidworks
+      public void OpenSolidworks() {
+        Console.WriteLine("Opening Solidworks.exe ...");
+
+        // // If any parts are open, notify and exit script
+        // if (swApp.ActiveDoc != null)
+        // {
+        //   Console.WriteLine("Solidworks already open...");
+        //   return;
+        // }
+        // else
+        // {
+        //   swApp.Visible = true;
+        //   Console.WriteLine("Opened Solidworks.exe successfully.");
+        // }
       }
     }
 */});
